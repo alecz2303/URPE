fa480a5cf00e5d2348b0b45c68f0c06f960194c7
Alejandro Rueda <alecz23@live.com>
2026-09-25 02:29:05 -0600

    URPE-32: expose score-linked HINE visuals

--- app/Http/Controllers/HineAssessmentController.php
+++ app/Http/Controllers/HineAssessmentController.php
@@ -123,6 +123,7 @@ class HineAssessmentController extends Controller
             'scores' => HineInstrument::URPE_SCORES,
             'anchors' => HineInstrument::clinicalAnchors(),
             'visuals' => HineInstrument::visualReferenceMap(),
+            'visualScoreFrames' => HineInstrument::visualScoreFrames(),
             'responses' => $assessment->hine->responses->keyBy('item_key'),
             'motorMilestones' => HineInstrument::motorMilestones(),
             'behaviorItems' => HineInstrument::behaviorItems(),
